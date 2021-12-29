bb831d246ba685958348d474a55403caa7ed89d2
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2021-12-29 14:43:14 +0100

    ⚡

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 const authRouter = require("./routes/auth");
 const jobRouter = require("./routes/jobs");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/job", jobRouter);
+
 app.get("/", (req, res) => {
   res.send("Jobs api");
 });
 
-const start = () => {
+const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(4000, () => {
       console.log("app running on port 4000");
     });
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
-
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
 const register = async (req, res) => {
-  res.send("Register User");
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
   res.send("Login User");
