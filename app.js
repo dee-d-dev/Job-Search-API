5342d7ad409b2b8865a1d991d003a4564973bd45
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2021-12-31 17:06:17 +0100

    end of the year commit, grateful for growth

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app = express();
 const authRouter = require("./routes/auth");
 const jobRouter = require("./routes/jobs");
 const connectDB = require("./db/connect");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
+
 require("dotenv").config();
 
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/job", jobRouter);
+app.use(errorHandlerMiddleware)
+app.use(notFound)
 
 app.get("/", (req, res) => {
   res.send("Jobs api");
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,32 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors/bad-request");
-const bcrypt = require("bcryptjs");
+const BadRequestError = require("../errors/bad-request");
+const UnauthenticatedError = require("../errors/unauthenticated");
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const register = async (req, res) => {
-  
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
+
 const login = async (req, res) => {
-  res.send("Login User");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  //find user by mail
+  const user = await User.findOne({ email });
+  //if user with such mail is not found
+  if (!user) {
+    throw new UnauthenticatedError("Provide Credentials ");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ name: user.name, token });
 };
 
 module.exports = { register, login };
